daf82e240eeb03ce249ff0594f35c5a4f1225d02
Mahim Chowdhury Miraj <176749866+mahimchowdhurymiraj@users.noreply.github.com>
2025-01-29 22:54:07 +0600

    Add files via upload

---
+++ maximum_finder_switchcase.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main()
+{
+    int num1, num2;
+
+    printf("Enter two numbers: ");
+    scanf("%d %d", &num1, &num2);
+
+    switch (num1 > num2)
+    {
+    case 1:
+        printf("Maximum: %d\n", num1);
+        break;
+    case 0:
+        switch (num1 < num2)
+        {
+        case 1:
+            printf("Maximum: %d\n", num2);
+            break;
+        case 0:
+            printf("Both numbers are equal.\n");
+            break;
+        }
+        break;
+    }
+
+    return 0;
+}
